b8371501f14d72941c7ab08fba5a7d5f5e29fce9
진현지 <jinhyunji520@gmail.com>
2025-06-19 15:47:39 +0900

    Add DataAssets
    For Quater View, Shoulder View

---
+++ Source/HackAndSlash/Character/HSCharacterControlData.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/HSCharacterControlData.h"
+
+UHSCharacterControlData::UHSCharacterControlData()
+{
+	TargetArmLength = 400.f;
+}
